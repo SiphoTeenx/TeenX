eea42a801d377b4b23491f78293f49dc66076b9a
RC_Student_lab <RC_Student_lab@10.0.0.110>
2024-09-10 12:29:28 +0000

    FirstCommit

---
+++ src/main/java/com/mycompany/notgit/NotGit.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.notgit;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class NotGit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
